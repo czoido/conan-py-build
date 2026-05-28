55eadc9df061bb779151913793d50cebceb9149a
Carlos Zoido <mrgalleta@gmail.com>
2026-05-28 07:02:19 +0200

    wip

--- src/conan_py_build/build.py
+++ src/conan_py_build/build.py
@@ -613,8 +613,11 @@ def _do_build_wheel(
         dirs_exist_ok=True,
     )
 
-    move_deploy_to_wheel(runtime_deploy_dir, staging_dir)
+    # Add rpath to extension modules only, we are not touching shared libs from Conan.
     patch_rpath(staging_dir)
+
+    # Copy shared libs from Conan's runtime_deploy to the wheel layout.
+    move_deploy_to_wheel(runtime_deploy_dir, staging_dir)
 
     # Create dist-info (or reuse the one prepared by prepare_metadata_for_build_wheel)
     if metadata_directory is not None:
